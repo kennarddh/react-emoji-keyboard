589e478dd553135cec9a4001526ee69026ede909
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-11-16 20:29:18 +0700

    feat: Preserve cursor position

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
 import Emoji from 'Constants/Emoji'
-import { FC, useCallback, useState, ChangeEvent, useEffect } from 'react'
+import {
+	FC,
+	useCallback,
+	useState,
+	ChangeEvent,
+	useEffect,
+	useRef,
+} from 'react'
 
 const App: FC = () => {
 	const [Text, SetText] = useState<string>('')
 	const [LastUsed, SetLastUsed] = useState<string[]>(() => {
 		return JSON.parse(localStorage.getItem('lastUsedEmojis') ?? '[]') ?? []
 	})
+	const [TargetIndex, SetTargetIndex] = useState<number>(0)
+
+	const InputRef = useRef<HTMLInputElement>(null)
 
 	const OnInput = useCallback((event: ChangeEvent<HTMLInputElement>) => {
+		let index = -1
+
 		const replaceResult = event.target.value.replaceAll(
 			/(:[^:]+:)/gi,
 			name => {
+				index = event.target.value.indexOf(name)
+
 				const emojiName = name.slice(1, -1).toLowerCase()
 
 				const findResult = Emoji.find(
@@ -38,11 +52,25 @@ const App: FC = () => {
 		)
 
 		SetText(replaceResult)
+		SetTargetIndex(index)
 	}, [])
 
 	useEffect(() => {
 		localStorage.setItem('lastUsedEmojis', JSON.stringify(LastUsed))
 	}, [LastUsed])
+
+	useEffect(() => {
+		if (TargetIndex === -1) {
+			InputRef.current?.setSelectionRange(
+				InputRef.current.value.length,
+				InputRef.current.value.length
+			)
+
+			return
+		}
+
+		InputRef.current?.setSelectionRange(TargetIndex + 1, TargetIndex + 1)
+	}, [TargetIndex])
 
 	return (
 		<div>
@@ -51,6 +79,7 @@ const App: FC = () => {
 				placeholder='Type :x:'
 				value={Text}
 				onChange={OnInput}
+				ref={InputRef}
 			/>
 			<div>
 				<h3>Last used emojis</h3>
